548947b33fca28d80265759d123f5483f4529b5b
tribuonoasto <tribuonoasto@gmail.com>
2022-09-14 21:52:02 +0700

    fitur login

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 'use strict'
 
 const express = require('express')
+const router = require('./routes')
 const app = express()
+const session = require('express-session')
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false,
+    sameSite: true 
+  }
+}))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ seeders/20220914130319-seedUser.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const fs = require ('fs')
+
+module.exports = {
+  up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const users = JSON.parse(fs.readFileSync('./data/users.json', 'utf-8'))
+      .map(el => {
+        el.createdAt = new Date ()
+        el.updatedAt = new Date ()
+        return el
+      })
+
+    return queryInterface.bulkInsert('Users', users)
+  },
+
+  down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+     return queryInterface.bulkDelete('Users', users)
+
+  }
+};
