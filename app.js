c11bb44e601d51f5b6a7791172cb123f15093423
tribuonoasto <tribuonoasto@gmail.com>
2022-09-14 18:45:32 +0700

    setup

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
