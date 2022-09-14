e874525b6dcd910e3945ac39919500e7d02c4f42
tribuonoasto <tribuonoasto@gmail.com>
2022-09-15 06:51:14 +0700

    migrasi

---
+++ migrations/20220914234740-addUserIdtoBooks.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return queryInterface.addColumn('Books', 'UserId', { type: Sequelize.INTEGER, references: {model: "Users"} })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     return queryInterface.removeColumn('Books', 'UserId')
+  }
+};
