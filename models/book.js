51cf97ccdcf1e92b47e4cacbc95459b54957b7b4
tribuonoasto <tribuonoasto@gmail.com>
2022-09-15 07:04:51 +0700

    assosiasi

--- models/book.js
+++ models/book.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Book.belongsTo(models.Category)
+      Book.belongsTo(models.User)
     }
   }
   Book.init({
