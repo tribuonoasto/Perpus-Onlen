991ebaa8d3e52ceea0e2da0b1b1c8cd7170f72ac
tribuonoasto <tribuonoasto@gmail.com>
2022-09-15 10:51:27 +0700

    uncomment routering autentivation

--- routes/books.js
+++ routes/books.js
@@ -3,27 +3,27 @@ const BookController = require('../controllers/bookcontroller')
 const router = express.Router()
 
 
-// router.use((req, res, next) => {
-//   if(!req.session.user || !req.session.user.id) {
-//     const error = 'Need to login'
-//     res.redirect (`/login?errors=${error}`)
-//   } else {
-//     next()
-//   }
-// })
+router.use((req, res, next) => {
+  if(!req.session.user || !req.session.user.id) {
+    const error = 'Need to login'
+    res.redirect (`/login?errors=${error}`)
+  } else {
+    next()
+  }
+})
 
 
 router.get('/', BookController.showAllBooks)
 
 
-// router.use((req, res, next) => {
-//   if(!req.session.user || req.session.user.role !== 'Admin') {
-//     const error = 'You have no access'
-//     res.redirect (`/login?errors=${error}`)
-//   } else {
-//     next()
-//   }
-// })
+router.use((req, res, next) => {
+  if(!req.session.user || req.session.user.role !== 'Admin') {
+    const error = 'You have no access'
+    res.redirect (`/login?errors=${error}`)
+  } else {
+    next()
+  }
+})
 
 router.get('/add', BookController.createBook)
 router.post('/add', BookController.saveBook)
